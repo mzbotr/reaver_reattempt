2e2706d02317c71af5df225c5b88c9b358475fbe
Oweoqi <anthony.cozamanis@chronicle.jp.net>
2014-05-27 14:52:11 +0800

    Update reaver_reattempt.sh

--- reaver_reattempt.sh
+++ reaver_reattempt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This script is used to change the Mac address of the wifi connection as well as the emulated one created by airmon-ng in an attempt to avoid being locked out of routers for repeated WPS attack attempts.
+# This script is used to change the Mac address of the wifi connection as well as the emulated one created by airmon-ng 
+# in an attempt to avoid being locked out of routers for repeated WPS attack attempts.
 # The reaver command runs once and then the script gives the machine a new Mac address. 
 
 while :; do echo
